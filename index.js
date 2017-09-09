585fbc2d85f24a4b12d5cb2f62fe0e25fd801fb7
Quintin Donnelly <quinndonnelly22@gmail.com>
2017-09-08 19:12:23 -0500

    Added docs orginized the code and removed data that should not be stored on git

--- index.js
+++ index.js
@@ -1,39 +1,97 @@
 const https = require('https');
 const util = require('util');
+const fs = require('fs');
 
+const DISCORD_WEBHOOK_JSON = 'discord-webhook.json';
 
-var postData = {
-  "username": "Gods of ACM",
-  "content": "Kyla is the most amazing girlfriend in the world!! @everyone",
+/**
+ * Will attempt to load the file given and parse as JSON.
+ * @param  {string} fileName The file you would like to open and be given as a
+ * json.
+ * @return {Promise}          A Promise that will hold the json object that was
+ * requestes by fileName or the error message if rejected
+ */
+async function getJSON(fileName) {
+  return new Promise((resolve, reject) => {
+    try {
+      fs.readFile(fileName, (err, buffer) => {
+        if (err) {
+          throw err;
+        }
+
+        let json = JSON.parse(buffer);
+        resolve(json);
+      });
+    } catch (err) {
+      let errorMsg = 'ERROR: unable to get ' + fileName + ' exiting\n' + err;
+      reject(errorMsg);
+    }
+  })
 }
 
-console.log("Data being sent: ", postData);
-var json = JSON.stringify(postData);
+/**
+ * Temporary function that makes the given post with the given username to the
+ * given webhook
+ * @param  {string} id       The webhook id
+ * @param  {string} token    The token for the webhook
+ * @param  {string} username The username to make the post with
+ * @param  {string} message  The post to makePost
+ * @return {Promise}          The promise will resolve empty on finish and
+ * reject with error message
+ */
+async function makePost(id, token, username, message) {
+  return new Promise((resolve, reject) => {
+    var postData = {
+      "username": username,
+      "content": message,
+    };
 
-const options = {
-  hostname: 'discordapp.com',
-  port: 443,
-  path: '/api/webhooks/355803885224591370/MLn6utYN4AY2vWKN9jynhQD7hp3CMA-OJ--VXvOp4uET2wOFXuLXNgBYKHdiF_hClJMc',
-  method: 'POST',
-  headers: {
-       'Content-Type': 'application/json',
-     }
-};
+    var json = JSON.stringify(postData);
 
+    const options = {
+      hostname: 'discordapp.com',
+      port: 443,
+      path: `/api/webhooks/${id}/${token}`,
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    };
 
-const req = https.request(options, (res) => {
-  console.log('statusCode:', res.statusCode);
-  console.log('headers:', res.headers);
+    try {
+      const req = https.request(options, (res) => {
+        if(res.statusCode > 300 || res.statusCode < 200) {
+          throw "statusCode: " + res.statusCode;
+        }
+      });
 
-  res.on('data', (d) => {
-    process.stdout.write(d);
+      req.write(json);
+
+      req.on('error', (e) => {
+        throw e;
+      });
+      req.on('end', ()=>{
+        resolve();
+      });
+
+      req.end();
+
+    } catch (err) {
+      reject(err)
+    }
+  })
+}
+
+/**
+ * This is the entry point for this node script
+ *
+ */
+async function main() {
+  let json = await getJSON(DISCORD_WEBHOOK_JSON).catch((err) => {
+    console.log(err)
   });
-});
 
+  makePost(json.id, json.token, "Gods of ACM", "Yo this is cool");
+}
 
-req.write(json);
-
-req.on('error', (e) => {
-  console.error(e);
-});
-req.end();
+main()
